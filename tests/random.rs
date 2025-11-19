fceb3bbc8c23e1427fb44ee1c298ebfb19c61c63
TheVeryDarkness <3266343194@qq.com>
2025-11-19 11:14:34 +0800

    Use 3-permutations for testing `String::from_boxed_str`

--- tests/random.rs
+++ tests/random.rs
@@ -1,18 +1,20 @@
-use random_string::generate_rng;
+use itertools::Itertools;
 use shared_vec::Counter;
 use std::cell::Cell;
-use std::iter;
 use std::sync::atomic::AtomicUsize;
 
-const CHARSET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789🦀";
-
 fn test_string<C: Counter<usize>>() {
-    let mut strings = iter::repeat_with(|| generate_rng(0..1024, CHARSET))
-        .take(1024)
-        .map(|s| shared_vec::String::<C>::from_boxed_str(s.into_boxed_str()))
-        .collect::<Vec<shared_vec::String<C>>>();
+    let mut strings = [
+        'a', 'b', '¥', '±', 'α', 'β', '₍', '₎', '₤', '€', '⻰', '⻳', '🦀', '🧐', '𝆒', '𝆓',
+    ]
+    .iter()
+    .copied()
+    .permutations(3)
+    .map(|s| shared_vec::String::<C>::from_boxed_str(String::from_iter(s).into_boxed_str()))
+    .collect::<Vec<shared_vec::String<C>>>();
 
     strings.sort_by(|a, b| a.as_str().cmp(b.as_str()));
+    assert!(strings.iter().all(|w| w.chars().count() == 3));
     assert!(strings.is_sorted());
 }
 
